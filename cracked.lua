5fac2348a7b7ed2603e7741488af364fb81a945f
ThePotRat <77865071+ThePotRat@users.noreply.github.com>
2024-03-16 11:01:55 -0400

    Update cracked.lua

--- cracked.lua
+++ cracked.lua
@@ -10,7 +10,799 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
-  wait(0.2)
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = true
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+  wait()
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
+local args = {
+
+    [1] = "gunpowder",
+
+    [2] = false,
+
+    [3] = false
+
+}
+
+game:GetService("ReplicatedStorage").Packages.Knit.Services.ShopService.RF.Shop:InvokeServer(unpack(args))
 local args = {
 
     [1] = "gunpowder",
